1d1138e2a935f21a0befd4f34b5616de8ecd8c6a
Türker YILDIRIM <me@turkeryildirim.com>
2025-05-28 16:01:06 +0300

    version history sample added

---
+++ samples/VersionHistory.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+require_once '../vendor/autoload.php';
+
+use Turker\FigmaAPI\Endpoints\VersionHistoryEndpoint;
+use Turker\FigmaAPI\FigmaClient;
+
+# file key that can be retrieved from other endpoints like FilesEndpoint
+$fileKey = 'my-file-key';
+
+# init FigmaClient with default cache and log driver by using API key
+$fc = FigmaClient::factory()->getWithApiKey('key');
+
+# init Version history endpoint
+$history = new VersionHistoryEndpoint($fc);
+
+# fetch all versions belongs to the file
+$allHistory = $history->fetchAll($fileKey);
+
+# get versions
+$versions = $allHistory->versions;
+
+# All fields will be populated with proper data. If no data returns from endpoint, props will be set
+# to default values which is defined in the official API documentation.
+# If there is no default value all missing props set as NULL.
+# e.g This endpoint does not return user email so it'll be set as NULL
+foreach ($versions as $version) {
+    $id = $version->id;
+    $description = $version->description;
+    $label = $version->label;
+    $createdAt = $version->createdAt;
+    $user = $version->user;
+    $userId = $user->id;
+    $imgUrl = $user->imgUrl;
+    $handle = $user->handle;
+    $email = $user->email;
+}
